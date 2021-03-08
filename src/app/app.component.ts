f02005b7b40e7dc73e1086591c651dd9b4cf5ff1
LaughingThroll <laughingthroll@gmail.com>
2021-03-08 14:29:18 +0200

    added: Themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+
 import { DateService } from './services/date/date.service'
 import { DataService } from './services/data/data.service'
-import { IMember, ITeam } from './interfaces/DB';
+
+import { THEMES } from './constant'
+import { ITeam } from './interfaces/DB'
+import { TThemes } from './interfaces/utils'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit  {
+export class AppComponent implements OnInit {
   public allDays: Date[] = []
   public teams: ITeam[] = []
+  public THEMES: TThemes[] = THEMES 
   public allDays$: Subscription | null = null
   public teams$: Subscription | null = null
 
-  constructor(private dateSevice: DateService, private dataService: DataService) {}
+  constructor(private dateSevice: DateService, private dataService: DataService) { }
 
   ngOnInit(): void {
     this.allDays$ = this.dateSevice.getDate().subscribe({
@@ -26,7 +32,7 @@ export class AppComponent implements OnInit  {
       next: teams => {
         this.teams = teams
       },
-      error: err => console.log('Custom Error', err) 
+      error: err => console.log('Custom Error', err)
     })
   }
 
---
+++ src/app/interfaces/utils.ts
@@ -0,0 +1,1 @@
+export type TThemes = [string, string]
