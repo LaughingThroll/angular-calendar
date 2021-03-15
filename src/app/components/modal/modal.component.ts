c586f266069258e17a4a222064302a7ef7fd918b
LaughingThroll <laughingthroll@gmail.com>
2021-03-15 10:50:17 +0200

    added: markup-modal

---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core'
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss'],
+  encapsulation: ViewEncapsulation.None
+
+})
+export class ModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
